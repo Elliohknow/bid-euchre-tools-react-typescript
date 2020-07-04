f61bc4f1096164599c276deba2f5a148a0bfc303
Elliott <elliottohj44@gmail.com>
2020-07-04 16:24:27 -0400

    added players, player cards, some localstorage data setting and getting functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,183 @@
 import CSS from "csstype";
-import React, { SyntheticEvent, useEffect, useState } from "react";
-import { getDateTime } from "./utils";
+import React, { SyntheticEvent, useState } from "react";
 
-const suits = {
-  SPADES: "♠",
-  HEARTS: "♥",
-  DIAMONDS: "♦",
-  CLUBS: "♣",
-};
-
-type RubberProps = {
-  date: string;
+type GameProps = {
   numPlayers: number;
+  date?: string;
   dealer?: string;
   highestBidder?: object;
-  players?: object;
+  players?: any;
+};
+type CardProps = {
+  player: Player;
+};
+type Player = {
+  name: string;
+  gamesPlayed: number;
+  wins: number;
+  longestWinStreak: number;
+  onWinstreakNow: boolean;
+};
+const dummy: Player = {
+  name: "Harry",
+  gamesPlayed: 0,
+  wins: 0,
+  longestWinStreak: 0,
+  onWinstreakNow: false,
 };
 
-const Rubber = ({ date, numPlayers, players }: RubberProps) => {
+const Rubber = ({ numPlayers, date, players = [dummy] }: GameProps) => {
+  const numDummies = numPlayers <= 4 ? 4 - numPlayers : 0;
+
   return (
-    <div className="">
+    <div style={rubberStyles}>
       <div>This is gonna be a Rubber, folks.</div>
       <div>Probably.</div>
-      <div className="date">{date}</div>
-      <div className="num-players">number of players: {numPlayers}</div>
-      <div className="num-dummies">number of dummy players: {numPlayers <= 4 ? 4 - numPlayers : 0}</div>
+      <div className="date">{date ? date : "the date string goes here"}</div>
+      <div className="numPlayers">number of players: {numPlayers}</div>
+      <div className="numDummies">number of dummy players: {numDummies}</div>
+      {players.map((player: Player) => (
+        <div key={`player_${player.name}`}>{player.name}</div>
+      ))}
     </div>
   );
 };
 
-const LoadGame = () => {};
+const Card = ({ player }: CardProps) => {
+  return (
+    <div className="float-button" style={cardStyles}>
+      <div>{player.name}</div>
+      <div>{player.gamesPlayed}</div>
+      <div>{player.wins}</div>
+      <div>{player.longestWinStreak}</div>
+      <div>{player.onWinstreakNow}</div>
+    </div>
+  );
+};
 
-const NewGame = () => {};
+const NewGame = () => {
+  const [players, setPlayers]: any = useState([]);
+  const [ready, setReady] = useState(false);
+  const numPlayers = 2 || players.length;
 
-export default function App() {
-  const [numPlayers, setNumPlayers] = useState(2);
-  const [date, setDate]: any = useState(null);
-
-  useEffect(() => setDate(getDateTime()));
-
+  const startGame = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setReady(true);
+  };
   const incrementPlayers = (e: SyntheticEvent) => {
     e.preventDefault();
-    setNumPlayers(numPlayers + 1);
+    let tempArray = players;
+    let newPlayer: Player = dummy;
+    newPlayer.name = "???";
+    tempArray.push(newPlayer);
+    setPlayers(tempArray);
   };
   const decrementPlayers = (e: SyntheticEvent) => {
     e.preventDefault();
-    setNumPlayers(numPlayers - 1);
+    let tempArray = players;
+    tempArray.pop();
+    setPlayers(tempArray);
+  };
+  return !ready ? (
+    <React.Fragment>
+      <div className="button-wrapper">
+        <button className="double-button" onClick={incrementPlayers} style={buttonStyles}>
+          + Player
+        </button>
+        <button className="double-button" onClick={decrementPlayers} style={buttonStyles}>
+          - Player
+        </button>
+      </div>
+      <button className="float-button" onClick={startGame} style={startButtonStyles}>
+        Start
+      </button>
+      <Rubber players={players} numPlayers={numPlayers} />
+    </React.Fragment>
+  ) : (
+    <div style={{ gridTemplateColumns: `repeat(${numPlayers}, 1fr)`, width: "100%" }}>
+      {players.map((player: Player) => (
+        <Card key={`card_${player.name}`} player={player} />
+      ))}
+    </div>
+  );
+};
+
+const LoadGame = () => {
+  return <div></div>;
+};
+
+export default function App() {
+  const [game, setGame] = useState("");
+
+  const handleNewGame = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setGame("NEW");
+  };
+  const handleLoadGame = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setGame("LOAD");
   };
 
   return (
     <div style={appStyles}>
-      eyyyyyy
-      <div className="button-wrapper">
-        <button onClick={incrementPlayers} style={{ fontWeight: "bold", backgroundColor: "royalblue", color: "white" }}>
-          + Player
-        </button>
-        <button onClick={decrementPlayers} style={{ fontWeight: "bold", backgroundColor: "royalblue", color: "white" }}>
-          - Player
-        </button>
-      </div>
-      <Rubber date={date} numPlayers={numPlayers} />
+      {!game && (
+        <React.Fragment>
+          <h1 style={h1Styles}>bid euchre tools</h1>
+          <div className="button-wrapper">
+            <button className="double-button" onClick={handleNewGame} style={buttonStyles}>
+              new game
+            </button>
+            <button className="double-button" onClick={handleLoadGame} style={buttonStyles}>
+              load game
+            </button>
+          </div>
+        </React.Fragment>
+      )}
+      {game === "NEW" && <NewGame />}
+      {game === "LOAD" && <LoadGame />}
     </div>
   );
 }
 
 const appStyles: CSS.Properties = {
+  backgroundColor: "#EEEEEE",
   display: "block",
+  height: "100vh",
   width: "100%",
   justifyContent: "space-between",
   textAlign: "center",
+  fontSize: "16px",
 };
+const h1Styles: CSS.Properties = {
+  textTransform: "capitalize",
+  marginTop: "0",
+};
+const buttonStyles: CSS.Properties = {
+  fontWeight: "bold",
+  textTransform: "uppercase",
+  color: "white",
+  backgroundColor: "cornflowerblue",
+  cursor: "pointer",
+};
+const startButtonStyles: CSS.Properties = {
+  fontWeight: "bold",
+  fontSize: "2em",
+  textTransform: "uppercase",
+  color: "royalblue",
+  backgroundColor: "white",
+  cursor: "pointer",
+};
+const rubberStyles: CSS.Properties = {
+  width: "100%",
+};
+const cardStyles: CSS.Properties = {
+  gridColumn: "span 1",
+};
+
+// const suits = {
+//   SPADES: "♠",
+//   HEARTS: "♥",
+//   DIAMONDS: "♦",
+//   CLUBS: "♣",
+// };
+// const deckSize = 32; // (4*4)*2
